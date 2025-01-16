687088ad78c200e193578952668a41b9f8504ff8
raeraecoder <khine521pmk@gmail.com>
2025-01-17 01:27:33 +0700

    do some change on core/deployment_settings.py

--- core/deployment_settings.py
+++ core/deployment_settings.py
@@ -2,10 +2,10 @@ import os
 import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
-ALLOWED_HOSTS=[os.environ.get["RENDER_EXTERNAL_HOSTNAME"]]
+ALLOWED_HOSTS=[os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
 CSRF_TRUSTED_ORIGINS=['https://'+os.environ.get["RENDER_EXTERNAL_HOSTNAME"]]
 DEBUG = False
-SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 MIDDLEWARE = [
      "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
@@ -35,6 +35,6 @@ DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         # default='postgresql://postgres:postgres@localhost:5432/mysite',
-        default=os.environ['DATABASE_URL']
+        default=os.environ('DATABASE_URL')
         conn_max_age=600)
 }
