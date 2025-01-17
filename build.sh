131999f23a82ba3f0c27ea57f1cfd820e8f058c6
raeraecoder <khine521pmk@gmail.com>
2025-01-18 00:50:23 +0700

    modify some sentence in buils.sh file

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 if [[$CREATE_SUPERUSER]];
-then py manage.py createsuperuser --no-input
+then 
+    python manage.py createsuperuser --no-input
 fi
