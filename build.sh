1072839611dd9c6ec86d20cbed40a0b1de9a01a1
raeraecoder <khine521pmk@gmail.com>
2025-01-19 01:17:56 +0700

    correct something

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]
+if [[ $CREATE_SUPERUSER ]]:
 then
-    python manage.py createsuperuser --noinput
+    python manage.py createsuperuser --no-input
 fi
